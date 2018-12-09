990eca7ac2b6cec1fa37c5d2253c5ced33ab0c42
Lachlan Kermode <lachiekermode@gmail.com>
2018-12-09 10:57:43 +0000

    kernel w basic vga driver

---
+++ qemu-run.sh
@@ -0,0 +1,1 @@
+qemu-system-x86_64 -drive format=raw,file=target/x86_64-lk_os/debug/bootimage-lk_os.bin
---
+++ src/main.rs
@@ -0,0 +1,20 @@
+#![feature(exclusive_range_pattern)]
+#![no_std]
+#![no_main]
+
+extern crate volatile;
+use core::panic::PanicInfo;
+
+mod vga_buffer;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    vga_buffer::print_something();
+
+    loop {}
+}
