7572babe5a254da7aaf92aaf4e45199e82466498
Lachlan Kermode <lachiekermode@gmail.com>
2018-12-15 14:53:58 +0000

    complete vga_buffer post

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,15 @@ mod vga_buffer;
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
+    panic!("some panic msg");
 
     loop {}
 }
