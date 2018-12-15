9eb54b5920e955f5da8467d7d1528377de1cec7f
Lachlan Kermode <lachiekermode@gmail.com>
2018-12-15 14:51:41 +0000

    println! macro

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,5 +1,21 @@
 use volatile::Volatile;
-// use spin::Mutex;
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
+}
 
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
